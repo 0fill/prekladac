1a0891554215979117fa3b21e133590e2ba8f351
0fill <fikrosek@seznam.cz>
2024-07-08 16:23:47 +0200

    new era
    come from tuple to list,
    still needs final touches,

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,5 +1,28 @@
 from translator import Translator
 
 
-t = Translator()
-t.show_10best()
+t =  Translator()
+
+
+while True:
+    print(f"welcome to my translator from cz to en enjoy\n\n1-translate\n2-add world\n3-delete translation\n4-show 10 best words"
+          f"\n5-show 10 worst words\n6-replace translation\n7-exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":       #translation
+        t.translate(t.find_czword())
+    elif choice == "2":
+        t.add_word(input("Enter a czech word: "),input("Enter english translation: "))
+    elif choice == "3":
+        t.remove_word(t.find_czword())
+    elif choice == "4":
+        t.del_translation(t.find_enword())
+    elif choice == "5":
+        t.show_10best()
+    elif choice == "6":
+        t.show_10worst()
+    elif choice == "7":
+        t.replace_translation(t.find_czword(), input("Enter a new translation: "))
+    elif choice == "8":
+        break
+    elif choice == '*':
+        t.print_data()
--- pythonProject/translator.py
+++ pythonProject/translator.py
@@ -1,85 +1,115 @@
 class Translator:
     def __init__(self):
-        self.data = {"jablko": ("apple", 5),
-                     "stůl": ("table", 8),
-                     "kočka": ("cat", 7),
-                     "kniha": ("book", 4),
-                     "auto": ("car", 5),
-                     "slunce": ("sun", 4),
-                     "dům": ("house", 8),
-                     "květina": ("flower", 2),
-                     "pes": ("dog", 5),
-                     "hory": ("mountains", 8),
-                     "řeka": ("river", 6),
-                     "most": ("bridge", 2),
-                     "pták": ("bird", 7),
-                     "ryba": ("fish", 2),
-                     "káva": ("coffee", 1),
-                     "telefon": ("phone", 9),
-                     "obloha": ("sky", 8),
-                     "tráva": ("grass", 1),
-                     "pláž": ("beach", 3),
-                     "město": ("city", 0),
-                     "strom": ("tree", 7),
-                     "hvězdy": ("stars", 58),
-                     "vítr": ("wind", 4),
-                     "sníh": ("snow", 4),
-                     "dešť": ("rain", 6),
-                     "kytara": ("guitar", 8),
-                     "cesta": ("road", 0),
-                     "kolo": ("bike", 4),
-                     "vlak": ("train", 7),
-                     "letadlo": ("airplane", 30),
-                     "oceán": ("ocean", 6),
-                     "ostrov": ("island", 3),
-                     "písek": ("sand", 7),
-                     "zahrada": ("garden", 2),
-                     "hora": ("mountain", 1),
-                     "louka": ("meadow", 2),
-                     "les": ("forest", 36),
-                     "jezero": ("lake", 4),
-                     "voda": ("water", 7),
-                     "měsíc": ("moon", 4),
-                     "planeta": ("planet", 1),
-                     "vesmír": ("universe", 9),
-                     "země": ("earth", 3),
-                     "vesnice": ("village", 2)
-                     }
+        self.data = {"jablko": [42, "apple"],
+                "kočka": [17, "cat"],
+                "strom": [91, "tree"],
+                "dům": [58, "house"],
+                "kolo": [73, "bicycle"],
+                "slunce": [29, "sun"],
+                "knihovna": [64, "library"],
+                "hory": [5, "mountains"],
+                "káva": [88, "coffee"],
+                "vlak": [12, "train"],
+                "dítě": [37, "child"],
+                "květina": [19, "flower"],
+                "voda": [76, "water"],
+                "čas": [81, "time"],
+                "pes": [66, "dog"],
+                "město": [94, "city"],
+                "most": [50, "bridge"],
+                "pláž": [3, "beach"],
+                "ryba": [8, "fish"],
+                "kniha": [25, "book"],
+                "okno": [70, "window"],
+                "křeslo": [45, "chair"],
+                "telefon": [99, "phone"],
+                "stůl": [14, "table"],
+                "papír": [31, "paper"],
+                "klobouk": [60, "hat"],
+                "dveře": [10, "door"],
+                "krabice": [47, "box"],
+                "kruh": [23, "circle"],
+                "hvězda": [84, "star"],
+                "kamion": [55, "truck"],
+                "písek": [36, "sand"],
+                "kostel": [68, "church"],
+                "hodiny": [97, "clock"],
+                "tráva": [2, "grass"],
+                "písmeno": [18, "letter"],
+                "kámen": [72, "stone"],
+                "kolo": [33, "wheel"],
+                "kabelka": [49, "purse"],
+                "křídlo": [7, "wing"],
+                "koule": [22, "ball"],
+                "křižovatka": [41, "intersection"],
+                "kružnice": [15, "circle"],
+                "koberec": [53, "carpet"],
+                "klíč": [90, "key"],
+                "křoví": [27, "bush"],
+                "křišťál": [79, "crystal"],
+                "kabát": [38, "coat"],
+                "křeslo": [11, "armchair"]}
+    def translate(self, word):
+        self.data.get(word)[0] += 1
+        return self.data.get([word])[1:]
+
+    # words operation
 
     def add_word(self, word, translation):
-        self.data.update({word: (translation, 0)})
-
-    def translate(self, word):
-        self.data.get(word)[1] += 1
-        return self.data.get([word])[0]
+        self.data.update({word: [0,translation]})
 
     def remove_word(self, word):
         self.data.pop([word])
 
-    def replace_translation(self, word, translation):
-        self.data.get(word)[0] = translation
+    def replace_word(self, word, new_word):
+        temp = self.data.get(word)
+        self.data.pop([word])
+        self.data.update({new_word: temp})
 
+    # translation operation
+
+    def add_translation(self, word, translation):
+        self.data.get(word).append(translation)
+
+    def replace_translation(self, word, translation):       #revork
+        self.data.get(word)[1] = translation
+
+    def del_translation(self, word):     #revork
+        del self.data[word][1]
+
+    # others
     def print_data(self):
         self.sort_data()
-        t = self.data
-        print(t)
-        print(t.__class__)
         for key in list(self.data.keys()):
-            print(f"{key} --> {self.data[key][0]} and was {self.data[key][1]} translated")
+            print(f"{key} --> {self.data[key][1:]} and was {self.data[key][0]} translated")
 
     def sort_data(self):
-        self.data = dict(sorted(self.data.items(), key=lambda x: x[1][1], reverse=True))       #doesnt work
+        self.data = dict(sorted(self.data.items(), key=lambda x: x[1][0], reverse=True))
 
-    def del_translation(self, word):
-        del self.data[word][0]
 
     def show_10best(self):
         self.sort_data()
         for i in range(10):
-            print(f"{i + 1}. {list(self.data.keys())[i]} --> {list(self.data.values())[i][0]} with popularity of {list(self.data.values())[i][1]}")
+            print(f"{i + 1}. {list(self.data.keys())[i]} --> {list(self.data.values())[i][1:]} with popularity of {list(self.data.values())[i][0]}")
 
     def show_10worst(self):
         self.sort_data()
         for i in range(10):
             print(
-                f"{i + 1}. {list(self.data.keys())[-i]} --> {list(self.data.values())[-i][0]} with popularity of {list(self.data.values())[-i][1]}")
+                f"{i + 1}. {list(self.data.keys())[-i]} --> {list(self.data.values())[-i][1:]} with popularity of {list(self.data.values())[-i][0]}")
+
+    #searchs
+    def find_czword(self,choice = input(f"what are you loking for?(cz) ")):
+        words = self.data.keys()
+        for m in words:
+            if choice in m:
+                if input(f"is this your choice {m}? (y/n) ").lower() == "y":
+                    return m
+
+    def find_enword(self,choice= input("what word are you looking for?(en) ")):
+        words = self.data.items()
+        for m in words:
+            for k in m[1:]:
+                if choice in k:
+                    if input(f"is this your choice {k}? (y/n) ").lower() == "y":
+                        return k
